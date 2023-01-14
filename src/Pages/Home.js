122408c02cd763636ef290f39e0c150c0a906dbf
An_34 <anna.villar34@gmail.com>
2023-01-14 20:55:49 +0100

    footer created

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,19 +7,19 @@ export const Home = () => {
   return (
     <>
 
-      <div class="flex mb-4 flex-col md:flex-row md:mt-16">
+      <div class="flex  flex-col md:flex-row">
         {/* columna texto */}
-          <div className="w-full md:w-1/2 h-9 pt-16 p-4 text-center md:mt-20 ">
-            <h1 className="font text-4xl lg:text-5xl font-bold tracking-tight text-pink-100 text-center">
+          <div className="w-full md:w-1/2 h-9 pt-16 p-4 text-center  ">
+            <h1 className="font text-4xl lg:text-6xl font-bold tracking-tight text-pink-100 text-center">
               Get the help you need, every step of the way
             </h1>
-            <p className="mt-4 text-xl lg:text-2xl md:p-4 text-pink-200">
+            <p className="mt-4 text-xl lg:text-3xl md:p-4 text-pink-200">
               Upgrade YOUR business for better future. Create or upgrade your websites and spend less with Essentials.
             </p>
           </div>
           
           {/* Columna imagen */}
-          <div className="mt-28 md:mt-0 p-8 md:w-1/2">
+          <div className="mt-48 md:mt-0 p-8 md:w-1/2">
             <div className="mt-10 md:mt-0">
               {/* Decorative image  */}
               <div className="h-100 w-100 overflow-hidden rounded-lg">
@@ -46,15 +46,84 @@ export const Home = () => {
             </div>
           </div>
         
-
-
-
-
       </div>
+{/* Test */}
 
 
 
-    </>
+<footer className="bg-gray-800 pt-10 sm:mt-10 bottom-0 mt-auto">
+    <div className="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
+      
+        
+
+        {/* <!-- Col-2 --> */}
+        <div className="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            {/* <!-- Col Title --> */}
+            <div className="text-xs uppercase text-gray-400 font-medium mb-6">
+                Core Concepts
+            </div>
+
+            {/* <!-- Links --> */}
+            <Link to="#" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 no-underline">
+                Utility-First
+            </Link>
+            <Link to="#" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 no-underline">
+                Responsive Design
+            </Link>
+            <Link to="#" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 no-underline">
+                Hover, Focus, & Other States
+            </Link>
+            <Link to="#" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 no-underline">
+                Dark Mode
+            </Link>
+           
+        </div>
+
+        
+
+        {/* <!-- Col-3 --> */}
+        <div className="p-5 w-1/2 sm:w-4/12 md:w-3/12  ">
+            {/* <!-- Col Title --> */}
+            <div className="text-xs uppercase text-gray-400 font-medium mb-6">
+                Community
+            </div>
+
+            {/* <!-- Links --> */}
+            <Link to="#" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 no-underline">
+                GitHub
+            </Link>
+            <Link to="#" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 no-underline">
+                Discord
+            </Link>
+            <Link to="#" className="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700 no-underline">
+                YouTube
+            </Link>
+        </div>
+    </div>
+
+    {/* <!-- Copyright Bar --> */}
+    <div class="pt-2">
+        <div class="flex pb-5 px-3 m-auto pt-5 
+            border-t border-gray-500 text-gray-400 text-sm 
+            flex-col md:flex-row max-w-6xl">
+            <div class="mt-2">
+                © Copyright 1998-year. All Rights Reserved.
+            </div>
+
+            {/* <!-- Required Unicons (if you want) --> */}
+           
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+
+    
+      </>
   )
 }
+
 
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -133,7 +133,7 @@ export const Main = () => {
         <div className="flex  flex-col  p-4  md:flex-row md:gap-4 justify-center">
 
           <div className="flex text-center flex-col   ">
-            <img src={fotoWeb} alt='web'className="rounded md:w-80" />
+            <img src={fotoWeb} alt='web' className="rounded md:w-80 mb-4" />
             <CheckBox
               label={"Web Page $500"}
               value={500}
@@ -155,7 +155,7 @@ export const Main = () => {
           )}
 
           <div className="flex text-center flex-col ">
-            <img src={fotoSeo} alt='Seo services' className="rounded  md:w-80" />
+            <img src={fotoSeo} alt='Seo services' className="rounded  md:w-80 mb-4" />
             <CheckBox
               label={"Seo Consulting $300"}
               value={300}
@@ -168,7 +168,7 @@ export const Main = () => {
 
 
           <div className="flex text-center flex-col">
-            <img src={fotoAds} alt='google computer'  className="rounded  md:w-80"/>
+            <img src={fotoAds} alt='google computer' className="rounded  md:w-80 mb-4" />
             <CheckBox
               label={"Google Ads Campaign $200"}
               value={200}
@@ -181,53 +181,71 @@ export const Main = () => {
         </div>
       </div>
       <div className="flex flex-col text-pink-100 text-center">
-        <h5>Total Price: {totalPrice}</h5>
-        <h3>Cliente</h3>
-              <div >
-                <label htmlFor="clientName" className="d-block">Nombre:</label>
-                <input
-                  placeholder="Nombre cliente"
-                  type="text"
-                  name="clientName"
-                  value={clientName}
-                  minLength="3"
-                  onChange={(e) => setClientName(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="budgetName" className="d-block">Nombre del presupuesto:</label>
-                <input
-                  placeholder="Nombre presupuesto"
-                  type="text"
-                  name="budgetName"
-                  value={budgetName}
-                  minLength="3"
-                  onChange={(e) => setBudgetName(e.target.value)}
-                />
-                <div>
-              <button onClick={submitData} className=" mt-3 justify-center inline-block rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-center font-medium text-white  hover:bg-indigo-700 ">Submit</button>  
-              </div>  
-              </div>
+        <h5 className="mb-4">Total Price: {totalPrice}</h5>
+        <h4 className="">Create your budget</h4>
+        <div >
+        <div className="flex justify-center mb-2">
+          <label htmlFor="clientName" className="d-block"></label>
+          <input
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-60 pl-10 p-1.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 d-block "
+            placeholder="Client name"
+            type="text"
+            name="clientName"
+            value={clientName}
+            minLength="3"
+            onChange={(e) => setClientName(e.target.value)}
+          />
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <label htmlFor="budgetName" className="d-block"></label>
+          <input 
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-60 pl-10 p-1.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 d-block " 
+            placeholder="Budget name"
+            type="text"
+            name="budgetName"
+            value={budgetName}
+            minLength="3"
+            onChange={(e) => setBudgetName(e.target.value)}
+          />
+        </div>
+          <div>
+            <button onClick={submitData} className=" mt-3 justify-center inline-block rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-center font-medium text-white  hover:bg-indigo-700 ">Submit</button>
+          </div>
       </div>
       <div className="flex flex-col text-pink-100 text-center">
-    <div className="mt-14">
-    <label htmlFor="findBudgetName" className="d-block mt-4">Find your budget:</label>
-            <input 
-            name="findBudgetName"
-            type='text'
-              onChange={handleChange}
-            ></input>
-      <div className="flex gap-2 justify-center mt-4 mb-4">
-      <button onClick={handleReset} className='flex rounded-md border border-transparent bg-indigo-600 py-1 px-4 text-center text-white  hover:bg-indigo-700'>Reset</button>
+        <div className="">
+
+          <label htmlFor="findBudgetName" className="d-block mt-4 justify-center"></label>
+          <div className="flex justify-center">
+            <div class="relative mt-1">
+              <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clip-rule="evenodd"></path>
+                </svg>
+              </div>
+              <input type="text" id="table-search" name="findBudgetName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-60 pl-10 p-1.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 d-block " placeholder="Find your budget" onChange={handleChange} />
+            </div>
+          </div>
+
+
+          <div className="flex gap-2 justify-center mt-4 mb-4">
+
+
+            <button onClick={handleReset} className='flex rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-center text-white  hover:bg-indigo-700'>Reset</button>
+
             <button onClick={handleSortName} className='flex rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-center text-white  hover:bg-indigo-700'>Order by name</button>
             <button onClick={handleSortDate} className='flex rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-center text-white  hover:bg-indigo-700'>Order by date</button>
 
-            </div>
-           
-            </div>
-            <BudgetsList budgetsList={budgetsList}
+          </div>
 
-            />
+        </div>
+        <BudgetsList budgetsList={budgetsList}
+
+        />
       </div>
     </>
   );
